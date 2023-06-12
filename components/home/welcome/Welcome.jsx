5d6331176edc734c85bd77f2011aefb39f68e562
blinkdeploys <blinkdeploys@gmail.com>
2023-06-11 20:07:55 -0700

    separate welcome and search

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -24,53 +24,8 @@ const Welcome = ({ searchTerm, setSearchTerm, handleClick }) => {
         >Hello Blink!</Text>
         <Text
           style={styles.welcomeMessage}
-        >Find your perfect job</Text>
+        >Polls.GH</Text>
       </View>
-
-      <View style={styles.searchContainer}>
-        <View style={styles.searchWrapper}>
-          <TextInput
-            style={styles.searchInput}
-            value={searchTerm}
-            onChangeText={(text) => setSearchTerm(text)}
-            placeholder="what are you looking for?"
-          />
-        </View>
-        <TouchableOpacity
-          style={styles.searchBtn}
-          onPress={handleClick}
-        >
-          <Image
-            source={icons.search}
-            resizeMode="contain"
-            style={styles.searchBtnImage}
-          />
-        </TouchableOpacity>
-      </View>
-
-      <View style={styles.tabsContainer}>
-        <FlatList
-          data={jobTypes}
-          keyExtractor={item => item}
-          contentContainerStyle={{ columnGap: SIZES.small }}
-          horizontal
-          renderItem={({ item })=> (
-            <TouchableOpacity
-              style={styles.tab(activeJobType, item)}
-              onPress={() => {
-                setActiveJobType(item);
-                router.push(`/search/${item}`);
-              }}
-            >
-              <Text
-                style={styles.tabText(activeJobType, item)}
-              >{item}</Text>
-            </TouchableOpacity>
-          )}
-          />
-      </View>
-
-
     </View>
   )
 }
