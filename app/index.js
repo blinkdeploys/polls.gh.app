22a0e9d070942564739b0fc5e5fc01367426a998
blinkdeploys <blinkdeploys@gmail.com>
2023-06-14 15:57:00 -0700

    index screen switch without react native nav

--- app/index.js
+++ app/index.js
@@ -1,60 +1,105 @@
-import { useState } from 'react'
-import { View, ScrollView, SafeAreaView, Text } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { View, ScrollView, SafeAreaView, Text, TouchableOpacity } from 'react-native'
 import { Stack, useRouter } from 'expo-router'
 import { COLORS, SIZES, icons, images } from '../constants'
 import {
-    ResultSheet, AgentActions, ECSummary, ScreenHeaderBtn, Welcome
+    ResultSheet, AgentActions, ECSummary, ScreenHeaderBtn, LoginScreen, Welcome
 } from '../components'
+import styles from './index.style'
 
 const Home = () => {
     const router = useRouter();
     const [searchTerm, setSearchTerm] = useState('')
+    const [token, setToken] = useState('')
     const [mode, setMode] = useState('')
+    const [isGuest, setIsGuest] = useState(true)
+
+    const fetchAsyncToken = async () => {
+        return await AsyncStorage.getItem('token')
+    }
+
+    const setAsyncToken = async (token) => {
+        if (typeof token === 'string') {
+            await AsyncStorage.setItem('token', token)
+        }
+    }
+
+    useEffect(() => {
+        let token = fetchAsyncToken()
+        if (token instanceof Promise) {
+            token = token.then((data) => {
+                setToken(data)
+                setAsyncToken(data)
+            }).catch((err) => console.log(err))
+        } else if (typeof token == 'string') {
+            setToken(token)
+        } else {
+            console.log('error fetching token... loggin out')
+        }
+    }, [])
+
+    useEffect(() => {
+        console.log(mode)
+        if (mode === 'logout') {
+            console.log('logging out....')
+            setToken('')
+            setAsyncToken('')
+        }
+    }, [mode])
+
+    useEffect(() => {
+        if (typeof token !== 'string') { setToken('') }
+        if (isValid(token)) {
+            setIsGuest(false)
+            console.log(`Token established! ${token}`)
+        } else {
+            setIsGuest(true)
+            console.log(`Not token found!`)
+        }
+        console.log("Is Guest: ", isGuest)
+    }, [token])
+
+    const handleLogin = (token) => {
+        setToken(token)
+        setMode('')
+    }
+
+    const handleLogout = () => {
+        setToken('')
+        setAsyncToken('')
+        setMode('')
+    }
+
+    const isValid = (item) => {
+        return !(['', undefined, null, false].includes(item))
+    }
 
     return (
-        <SafeAreaView
-            style={{ flex: 1, backgroundColor: COLORS.lightWhite }}
-        >
+        <SafeAreaView style={styles.container}>
             <Stack.Screen
                 options={{
-                    headerStyle: { backgroundColor: COLORS.lightWhite },
+                    headerStyle: styles.headerStyle,
                     headerShadowVisible: false,
-                    headerLeft: () => (
-                        <ScreenHeaderBtn iconUrl={icons.menu}
-                            dimension="60%"
-                            handlePress={() => setMode('')} />
-                    ),
-                    headerRight: () => (
-                        <ScreenHeaderBtn iconUrl={images.profile} dimension="80%" />
-                    ),
+                    headerLeft: () => isGuest
+                            ? <></>
+                            : (<ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" handlePress={() => setMode('')} />),
+                    headerRight: () => isGuest
+                            ? <></>
+                            : (<ScreenHeaderBtn iconUrl={images.profile} dimension="80%" />),
                     headerTitle: () => (
                         <View>
-                            <Text
-                                style={{
-                                    fontWeight: 'bold', fontSize: 18,
-                                    paddingHorizontal: 10,
-                                }}
-                            >Polls.GH</Text>
+                            <Text style={styles.logoText}>Polls.GH</Text>
                         </View>
                     )
                 }}
-            />
-            <ScrollView showVerticalScrollIndicator={false}>
-                <View
-                    style={{
-                        flex: 1,
-                        paddingHorizontal: SIZES.medium
-                    }}
+                />
+            {!isValid(token) && <LoginScreen onLogin={handleLogin} />}
+
+            {isValid(token) && <ScrollView
+                showVerticalScrollIndicator={false}
+                style={styles.scrollViewContainer}
                 >
-                    {/*<Welcome
-                        searchTerm={searchTerm}
-                        setSearchTerm={setSearchTerm}
-                        handleClick={() => {
-                            if (searchTerm) {
-                                router.push(`/search/${searchTerm}`)
-                            }
-                        }}
-                    />*/}
                     {(mode === 'presidential_sheet')
                         ? <ResultSheet
                             title="Presidential Results"
@@ -83,15 +128,10 @@ const Home = () => {
                             goHome={() => setMode('parliamentary_sheet')}
                             />
                         : <></>}
-                    {(mode === '')
-                        ? <AgentActions selectMode={setMode} />
-                        : <></>}
-                </View>
-            </ScrollView>
+                    {!isValid(mode) && <AgentActions selectMode={setMode} />}
+            </ScrollView>}
         </SafeAreaView>
     );
 }
 
 export default Home;
-
-
