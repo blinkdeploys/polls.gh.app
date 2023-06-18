c905f8ada3abe5512632f44e0de15fde435407de
blinkdeploys <blinkdeploys@gmail.com>
2023-06-17 21:20:02 -0700

    hook for fetching csrf token

---
+++ hook/useCsrfToken.js
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import { URL_LOCALHOST } from './constants'; 
+
+
+const useCsrfToken = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const getCsrfToken = async () => {
+    setLoading(true);
+    setError(null);
+    const endpoint = 'csrf'
+    const url = `${URL_LOCALHOST}${endpoint}/`
+    // console.log('Fetching csrf token', url)
+    try {
+        const response = await fetch(url, {
+                                        'method': 'GET',
+                                        'headers': {
+                                            'Content-Type': 'application/json',
+                                        },
+                                    });
+        if (!response.ok) {
+            throw new Error('CSRF Token fetch failed');
+        }
+        const data = await response.json();
+        return data.csrfToken;
+    } catch (error) {
+        console.log(error.message)
+        setError(error.message);
+        return null;
+    } finally {
+        setLoading(false);
+    }
+  };
+
+  return { loading, error, getCsrfToken };
+};
+
+export default useCsrfToken;
