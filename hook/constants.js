4d1034f86ce77565d918218cff52830911b6cec0
blinkdeploys <blinkdeploys@gmail.com>
2023-06-17 21:19:37 -0700

    constants for api urls

---
+++ hook/constants.js
@@ -0,0 +1,11 @@
+const URL = 'https://app.blinkdeploys-env.blinkdeploys.arc.domains/api/poll/app/'
+// const URL_LOCALHOST = 'http://127.0.0.1:50002/api/poll/app/'
+// 'https://www.localhost.architect.sh/api/poll/app/'
+const URL_LOCALHOST = 'http://10.0.0.100:50002/api/poll/app/'
+
+
+export {
+    URL,
+    URL_LOCALHOST,
+};
+  
