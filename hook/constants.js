c22b118d7058192a7ac6b0ec7e733c3ddd187d13
blinkdeploys <blinkdeploys@gmail.com>
2023-06-20 11:59:49 -0700

    constants URL_LOCAHOST in use (tmp)

--- hook/constants.js
+++ hook/constants.js
@@ -1,7 +1,8 @@
 const URL = 'https://app.blinkdeploys-env.blinkdeploys.arc.domains/api/poll/app/'
 // const URL_LOCALHOST = 'http://127.0.0.1:50002/api/poll/app/'
 // 'https://www.localhost.architect.sh/api/poll/app/'
-const URL_LOCALHOST = 'http://10.0.0.100:50002/api/poll/app/'
+// const URL_LOCALHOST = 'http://10.0.0.100:50002/api/poll/app/'
+const URL_LOCALHOST = 'https://app.blinkdeploys-env.blinkdeploys.arc.domains/api/poll/app/'
 
 
 export {
