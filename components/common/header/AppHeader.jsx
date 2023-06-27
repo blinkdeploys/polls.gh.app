cdaaef6ad3ae9e41dc50684c2ca6c56ebfcc6355
blinkdeploys <blinkdeploys@gmail.com>
2023-06-27 13:42:45 -0700

    correct task props and replace total_votes

---
+++ components/common/header/AppHeader.jsx
@@ -0,0 +1,38 @@
+import { View, Modal, TextInput, StyleSheet, Text, TouchableOpacity, ActivityIndicator } from 'react-native'
+import { AntDesign } from '@expo/vector-icons';
+import { COLORS, SIZES } from '../../../constants'
+import styles from '../../../components/home/action/agentActions.style'
+
+const AppHeader = ({ user, goHome, title }) => {
+    return (<View style={{ ...styles.header, flexDirection: 'row' }}>
+        <TouchableOpacity
+            style={{
+                width: '20%',
+                backgroundColor: COLORS.white,
+                borderRadius: 10,
+                padding: 10,
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}
+        >
+            {goHome && <Text
+                style={{ ...styles.headerBtn, color: '#000000' }}
+                onPress={goHome}
+            >Back</Text>}
+            {!goHome && <AntDesign name="ellipsis1" size={24} color="black" />}
+        </TouchableOpacity>
+        <View
+        style={{
+            width: '90%',
+            paddingHorizontal: 20,
+            paddingBottom: 20,
+        }}
+        >
+        <Text style={styles.headerTitle}>{title}</Text>
+        <Text style={styles.headerMedium}>{user.zone.station_code} {user.zone.station_title}</Text>
+        <Text style={styles.headerMedium}>{user.zone.constituency_title} Constituency</Text>
+        <Text style={styles.headerMedium}>{user.zone.region_title} Region</Text>
+        </View>
+    </View>)
+}
+export default AppHeader;
