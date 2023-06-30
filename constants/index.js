06128513b7399f5d24b8dc0bc1cb6028a892fe3a
blinkdeploys <blinkdeploys@gmail.com>
2023-06-30 11:44:09 -0700

    move constants to index file

--- constants/index.js
+++ constants/index.js
@@ -2,4 +2,23 @@ import images from "./images";
 import icons from "./icons";
 import { COLORS, FONT, SIZES, SHADOWS } from "./theme";
 
-export { images, icons, COLORS, FONT, SIZES, SHADOWS };
+// 'http://127.0.0.1:50002', 'https://www.localhost.architect.sh'
+const URL_BASE = 'https://app.blinkdeploys-env.blinkdeploys.arc.domains'
+// const URL_BASE = 'http://10.0.0.100:50002'
+const URL_API = `${URL_BASE}/api/poll/app`
+// async storage keys
+const ASKEY_TOKEN = 'auth_token'
+const ASKEY_CSRF_TOKEN = 'csrf_token'
+const ASKEY_PROFILE = 'user_profile'
+const ASKEY_URL = 'api_url'
+const ASKEY_PRESIDENTIAL = `presidential_sheet`
+const ASKEY_PARLIAMENTARY = `parliamentary_sheet`
+
+
+
+export {
+    URL_BASE, URL_API,
+    ASKEY_URL, ASKEY_TOKEN, ASKEY_CSRF_TOKEN, ASKEY_PROFILE,
+    ASKEY_PRESIDENTIAL, ASKEY_PARLIAMENTARY,
+    images, icons, COLORS, FONT, SIZES, SHADOWS
+};
